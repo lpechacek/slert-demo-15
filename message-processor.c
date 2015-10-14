dc2668c035d57d2747dd7e1b3495698ce4cc42e2
Libor Pechacek <lpechacek@suse.com>
2015-10-14 17:25:34 +0200

    message-processor: calculate processing delay

--- message-processor.c
+++ message-processor.c
@@ -5,10 +5,37 @@
 
 #include "shared_defs.h"
 
+/* Subtract the ‘struct timespec’ values X and Y, storing the result in RESULT.
+   Return 1 if the difference is negative, otherwise 0. */
+
+int timespec_substract(struct timespec *result, struct timespec *x, struct timespec *y)
+{
+	/* Perform the carry for the later subtraction by updating y. */
+	if (x->tv_nsec < y->tv_nsec) {
+		int nsec = (y->tv_nsec - x->tv_nsec) / 1000000000 + 1;
+		y->tv_nsec -= 1000000000 * nsec;
+		y->tv_sec += nsec;
+	}
+	if (x->tv_nsec - y->tv_nsec > 1000000000) {
+		int nsec = (x->tv_nsec - y->tv_nsec) / 1000000000;
+		y->tv_nsec += 1000000000 * nsec;
+		y->tv_sec -= nsec;
+	}
+
+	/* Compute the time remaining to wait.
+	   tv_nsec is certainly positive. */
+	result->tv_sec = x->tv_sec - y->tv_sec;
+	result->tv_nsec = x->tv_nsec - y->tv_nsec;
+
+	/* Return 1 if result is negative. */
+	return x->tv_sec < y->tv_sec;
+}
+
 int main(void)
 {
 	int msgq_id;
 	struct rt_msgbuf shmem_msg;
+	struct timespec ts_now, timediff;
 
 	msgq_id = msgget(MSGQ_KEY, IPC_CREAT | 0660);
 	if (msgq_id < 0) {
@@ -25,7 +52,14 @@ int main(void)
 		if (ret < 0)
 			perror("error upon IPC message receive");
 
-		/* TODO calculate times */
+		ret = clock_gettime(CLOCK_MONOTONIC, &ts_now);
+		if (ret < 0)
+			perror("error upon clock_gettime");
+
+		ret = timespec_substract(&timediff, &ts_now, &shmem_msg.recv_ts);
+
+		/* TODO pass the result on to logging process */
+		printf("%s%d.%09d\n", ret ? "-" : "", timediff.tv_sec, timediff.tv_nsec);
 	}
 }
 
