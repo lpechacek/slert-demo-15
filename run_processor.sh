ac02b8e4ec184017f9adbd47e4e181388b173a62
Mike Friesenegger <miked@friesenegger.us>
2015-10-19 20:52:08 -0600

    Enhanced run_processor.sh

--- run_processor.sh
+++ run_processor.sh
@@ -1,10 +1,32 @@
 #!/bin/bash
 
-# CPUs 1, 7, 13, 19 have been isolated
-taskset -c 1 ./network-listener &
+# Check if user cpuset is available
+if [ ! -e  /sys/fs/cgroup/cpuset/user/cpuset.cpus ]; then
+  echo "'user' cpuset is not available."
+  echo "Create cpuset using 'cset shield --kthread=on --cpu=X-Y'."
+  echo "   where x and y are valid cpu numbers for this system."
+  exit 1
+fi
+
+# Usage
+if test -z "$1" -o -z "$2"; then
+  echo "This script will (TO BE COMPLETED LATER)."
+  echo
+  echo "Usage: "$(basename $0" <CPU# FOR LISTENER> <CPU# FOR PROCESSOR>")
+  exit 1
+fi
+
+./network-listener &
 LISTENER_PID=$!
-taskset -c 7 ./message-processor &
+./message-processor &
 PROCESSOR_PID=$!
+echo "LISTENER_PID: $LISTENER_PID"
+echo "PROCESSOR_PID: $PROCESSOR_PID"
+cset proc -m "$LISTENER_PID" user
+taskset -cp "$1" $LISTENER_PID
+cset proc -m "$PROCESSOR_PID" user
+taskset -cp "$2" $PROCESSOR_PID
+
 trap "kill $LISTENER_PID; kill $PROCESSOR_PID" EXIT
 
 ./result-logger
