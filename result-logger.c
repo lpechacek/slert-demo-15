624fd06fa2308d69826e050ba4074afb0b6a6d5e
Libor Pechacek <lpechacek@suse.com>
2015-10-27 14:37:44 +0100

    result-logger: flush stdout whenever possible

--- result-logger.c
+++ result-logger.c
@@ -8,7 +8,7 @@
 
 int main(void)
 {
-	int logq_id, cntr;
+	int logq_id, msgrcv_flags, cntr;
 	struct rt_logbuf shmem_msg_log;
 
 	logq_id = msgget(LOGQ_KEY, IPC_CREAT | 0660);
@@ -20,12 +20,26 @@ int main(void)
 	for (cntr = 0;; cntr++) {
 		int ret;
 
-		/* we take any message from the queue regardless its length */
-		ret = msgrcv(logq_id, (struct msgbuf *)&shmem_msg_log,
-				sizeof(shmem_msg_log) - sizeof(shmem_msg_log.mtype),
-				0, MSG_NOERROR);
-		if (ret < 0 && errno != EINTR)
-			perror("error upon IPC message receive");
+		/* first check if there is a message in the queue */
+		/* if it's empty we have time for fflush */
+		msgrcv_flags = MSG_NOERROR | IPC_NOWAIT;
+		do {
+			ret = msgrcv(logq_id, (struct msgbuf *)&shmem_msg_log,
+					sizeof(shmem_msg_log) - sizeof(shmem_msg_log.mtype),
+					0, msgrcv_flags);
+
+			if (ret < 0) {
+				/* queue empty - flush output */
+				if (errno == ENOMSG && (msgrcv_flags & IPC_NOWAIT)) {
+					fflush(stdout);
+				} else {
+					if (errno != EINTR)
+						perror("error upon IPC message receive");
+				}
+			}
+
+			msgrcv_flags &= ~IPC_NOWAIT;
+		} while (ret < 0);
 
 		printf("\t0:\t%d:\t%s%d.%03d\n", cntr, shmem_msg_log.negative ? "-" : "",
 				shmem_msg_log.timediff.tv_sec * 1000000UL +
